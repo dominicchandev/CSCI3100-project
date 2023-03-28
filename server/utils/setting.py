ec7b4363e9aaf724c2d4c7d26342905d636c103f
ChanTinLokDominic <chantinlok2000@gmail.com>
2023-03-29 02:07:18 +0800

    Corrected type for token expire time

--- server/utils/setting.py
+++ server/utils/setting.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 class Setting:
     def __init__(self) -> None:
-        self.SECRET_KEY = os.environ.get('SECRET_KEY')
-        self.HASH_ALGORITHM = os.environ.get('ALGORITHM')
-        self.AWS_POSTGRES_URL = os.environ.get('AWS_POSTGRES_URL')
-        self.ACCESS_TOKEN_EXPIRE_MINUTES = os.environ.get('ACCESS_TOKEN_EXPIRE_MINUTES')
+        self.SECRET_KEY: str = os.environ.get('SECRET_KEY')
+        self.HASH_ALGORITHM: str = os.environ.get('ALGORITHM')
+        self.AWS_POSTGRES_URL: str = os.environ.get('AWS_POSTGRES_URL')
+        self.ACCESS_TOKEN_EXPIRE_MINUTES: float = float(os.environ.get('ACCESS_TOKEN_EXPIRE_MINUTES'))
