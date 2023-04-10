79934d5e844847ace848436d41993d91e1a68c2d
dominicchan <chantinlok2000@gmail.com>
2023-04-11 00:06:25 +0800

    API: search courses

--- server/crud/course.py
+++ server/crud/course.py
@@ -11,11 +11,46 @@ class CourseCRUD(CRUDBase):
     def __init__(self):
         super().__init__(model=CoursesModel)
     
-    def read(self, db: Session, course_id: str):
-        db_course = db.query(self.model).options(noload(self.model.users)).get(course_id)
+    def read(self, db: Session, id: str):
+        db_course = db.query(self.model).options(noload(self.model.users)).get(id)
         if not db_course:
             return None
         return db_course
+    
+    def read_by_name(self, db: Session, course_name: str):
+        db_course = db.query(self.model).options(noload(self.model.users)).filter(self.model.name == course_name).first()
+        if not db_course:
+            return None
+        return db_course
+    
+    def read_by_department(self, db: Session, department: str):
+        db_courses = db.query(self.model).options(noload(self.model.users)).filter(self.model.department == department).all()
+        return db_courses
+    
+    def read_by_class_time(self, db: Session, day: str, start_time: str, end_time: str):
+        course_ids = set()
+        id_schedule_entities: list = db.query(self.model).with_entities(self.model.id, self.model.schedule).all()
+        for id_schedule_entity in id_schedule_entities:
+            course_id = id_schedule_entity[0]
+            schedule = id_schedule_entity[1]
+            TIME_MATCH = False
+            for course_day in schedule:
+                if day == course_day:
+                    for place in schedule[course_day]:
+                        course_start_time = schedule[course_day][place][0]
+                        course_end_time = schedule[course_day][place][1]
+                        if course_start_time == start_time and course_end_time == end_time:
+                            course_ids.add(course_id)
+                            TIME_MATCH = True
+                            break
+                    if TIME_MATCH:
+                        break
+        
+        db_courses = []
+        for course_id in course_ids:
+            db_courses.append(self.read(db=db, id=course_id))
+        print(db_courses)
+        return db_courses
     
     def read_all(self, db: Session):
         all_courses = db.query(self.model).options(noload(self.model.users)).all()
