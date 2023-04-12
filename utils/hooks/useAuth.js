7343062741338f7b118e492a4df0d814842905d8
dominicchan <chantinlok2000@gmail.com>
2023-04-12 15:24:07 +0800

    Clean useAuth logic and added data

--- utils/hooks/useAuth.js
+++ utils/hooks/useAuth.js
@@ -1,50 +1,58 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 /**
  *
  * authStatus: "loading" | "unauth" | "auth"
- */
+*/
 export function useAuth() {
-  const [authStatus, setAuthStatus] = useState("loading");
   const [token, setToken] = useState("");
+  const token_expired = useRef(false)
+  const [authStatus, setAuthStatus] = useState("loading");
   const [courses, setCourses] = useState([]);
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
-
+  const [role, setRole] = useState("");
+  const [userId, setUserId] = useState("")
+  
   useEffect(() => {
-    // try {
-    (async () => {
-      // Check token exists
-      const token = localStorage.getItem("accessToken");
-      if (!token) {
-        setAuthStatus("unauth");
-        return;
-      }
-
-      const result = await fetch(`http://localhost:8000/api/users/me`, {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-
-      const resultJson = await result.json();
-      console.log(`Read me result:`);
-      console.log(resultJson);
-      setToken(token);
-      // setAuthStatus("auth");
-      setCourses(resultJson.courses);
-      setEmail(resultJson.email);
-      setName(resultJson.name);
-    })();
-  }, []);
-
-  useEffect(() => {
-    if (token != "") {
+    if (token != "" && token_expired.current === false) {
       setAuthStatus("auth")
     }
+    if (token_expired.current == true) {
+      setAuthStatus("unauth")
+    }
   }, [token])
-  // return { authStatus, accessToken: token, email, courses, name };
-  return { authStatus, token, email, courses, name };
+
+  // Check token exists
+  useEffect(() => {
+    const local_token = localStorage.getItem("accessToken");
+    if (!local_token) {
+      setAuthStatus("unauth");
+    }
+  
+    fetch(`http://localhost:8000/api/users/me`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${local_token}`,
+      },
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      token_expired.current = false;
+      setCourses(data.courses);
+      setEmail(data.email);
+      setName(data.name);
+      setRole(data.role);
+      setUserId(data.userId);
+    })
+    .catch((err) => {
+      token_expired.current = true;
+    })
+    .finally(() => {
+      setToken(local_token)
+    })
+  }, [])
+  
+  return { authStatus, token, email, courses, name , role, userId};
 
 }
