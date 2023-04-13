293d2f00ff74b6aacf580c76bfe7830737f89f7c
dominicchan <chantinlok2000@gmail.com>
2023-04-13 23:54:59 +0800

    Login related page ui responsiveness

--- pages/login/verify.js
+++ pages/login/verify.js
@@ -83,65 +83,66 @@ export default function LoginPage() {
     }
 
     return (
-        <Flex>
-                <Box w="50%" h='100%' bg='white'>
-                    <VStack>
-                        <NavigationBar colorMode={colorMode}/>
+        <Flex bg="white">
+            <Box w="50%" h='100vh' pr="20px" pl="20px">
+                <VStack>
+                    <NavigationBar colorMode={colorMode}/>
 
-                        <VStack pt = "100px" spacing ="10px" alignItems="left"
-                        alignContent="left"
-                        textAlign="left">
-                        <Text lineHeight="1.3" fontWeight="bold" fontSize="32px" color="#40DDCF" >
-                        Let us know it's you
-                        </Text>
-                        <Text
+                    <VStack pt = "100px" spacing ="10px" alignItems="left"
+                    alignContent="left"
+                    textAlign="left">
+                    <Text lineHeight="1.3" fontWeight="bold" fontSize="32px" color="#40DDCF" >
+                    Let us know it's you
+                    </Text>
+                    <Text
+                    lineHeight="1.4"
+                    fontWeight="bold"
+                    fontSize="14px"
+                    color="#A0AEC0"
+                    maxWidth="100%"
+                    textAlign="left"
+                    >
+                    To secure your account, enter the code we just sent to {email}
+                    </Text>
+                    <form onSubmit={handleSubmit}>
+                        <FormControl>
+                            <FormLabel htmlFor="code"fontFamily="Helvetica" lineHeight="1.4" fontSize="14px" color="Gray.Gray-700" width="35.5px" height="19.5px" >Code</FormLabel>
+                            <Input
+                                placeholder='Enter Code'
+                                fontSize="12px"
+                                type="text"
+                                id="code"
+                                value={code}
+                                onChange={(e) => setCode(e.target.value)}
+                            />
+                        </FormControl>
+                    </form>
+                        <Text color="red" fontSize="sm">{errMsg}</Text>
+                    <Spacer />
+                    <Button type="submit" bg='cyanAlpha' color = "white" variant = "solid" onClick={handleSubmit} isLoading={isLoading}>
+                        CONTINUE
+                    </Button>
+                    <Text
+                        fontFamily="Helvetica"
                         lineHeight="1.4"
-                        fontWeight="bold"
+                        fontWeight="regular"
                         fontSize="14px"
-                        color="#A0AEC0"
-                        maxWidth="100%"
-                        textAlign="left"
-                        >
-                        To secure your account, enter the code we just sent to {email}
-                        </Text>
-                        <form onSubmit={handleSubmit}>
-                            <FormControl>
-                                <FormLabel htmlFor="code"fontFamily="Helvetica" lineHeight="1.4" fontSize="14px" color="Gray.Gray-700" width="35.5px" height="19.5px" >Code</FormLabel>
-                                <Input
-                                    w = "300px"
-                                    placeholder='Enter Code'
-                                    fontSize="12px"
-                                    type="text"
-                                    id="code"
-                                    value={code}
-                                    onChange={(e) => setCode(e.target.value)}
-                                />
-                            </FormControl>
-                        </form>
-                            <Text color="red" fontSize="sm">{errMsg}</Text>
-                        <Spacer />
-                        <Button type="submit" bg='cyanAlpha' color = "white" variant = "solid" onClick={handleSubmit} isLoading={isLoading}>
-                            CONTINUE
-                        </Button>
-                        <Text
-                            fontFamily="Helvetica"
-                            lineHeight="1.4"
-                            fontWeight="regular"
-                            fontSize="14px"
-                            color="Gray.Gray-400"
-                            width="220.5px"
-                            height="19.5px"
-                        >
-                        <span>Didn't get the code? </span>
-                        <Box as="span" fontWeight="bold" color="#40DDCF">
-                        <Link fontSize="sm" onClick={handleClick}>Resend Code</Link>
-                        </Box>
-                        </Text>
-                        </VStack>
-                        </VStack>
-                        </Box>
-                        <Spacer/>
+                        color="Gray.Gray-400"
+                        width="220.5px"
+                        height="19.5px"
+                    >
+                    <span>Didn't get the code? </span>
+                    <Box as="span" fontWeight="bold" color="#40DDCF">
+                    <Link fontSize="sm" onClick={handleClick}>Resend Code</Link>
+                    </Box>
+                    </Text>
+                    </VStack>
+                    </VStack>
+            </Box>
+            <Spacer/>
+            <Box w="50%">
                 <CompanyIntro colorMode={colorMode}/>
-            </Flex>
+            </Box>
+        </Flex>
     );
 };
